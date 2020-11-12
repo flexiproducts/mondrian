d9ecf8843e2d8648fcba9c2245328fc28aa5a017
Finn Pauls <mbp@pauls.dev>
2020-11-12 22:17:16 +0000

    Basic Mondrian rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
+import mondrian from '../example.json'
 
 export default function App() {
-  return <Main>a nice new project</Main>
+  return (
+    <Main>
+      <Painting />
+    </Main>
+  )
 }
 
 const Main = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
   height: 100%;
-
-  background-color: #eeeee4;
   color: #242422;
   font-size: 75px;
+`
+
+function Painting() {
+  return (
+    <SvgOuter
+      version="1.1"
+      baseProfile="full"
+      width={mondrian.width}
+      height={mondrian.height}
+    >
+      {mondrian.rectangles.map((rectangle) => (
+        <rect
+          key={rectangle.index}
+          x={rectangle.x}
+          y={rectangle.y}
+          height={rectangle.height}
+          width={rectangle.width}
+          fill={rectangle.color_id}
+          stroke={'black'}
+          strokeWidth="10"
+        ></rect>
+      ))}
+    </SvgOuter>
+  )
+}
+
+const SvgOuter = styled.svg`
+  overflow: visible;
+  padding: 5px;
+  margin: 20px 0px;
 `
