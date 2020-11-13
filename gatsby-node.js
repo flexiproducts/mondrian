703d06355fbd3efe173bc81c8b44d43651cba8a3
Finn Pauls <mbp@pauls.dev>
2020-11-13 19:59:55 +0000

    Move components to path that are not managed by gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,14 +4,14 @@ exports.createPages = async ({actions: {createPage}}) => {
   mondrians.forEach((mondrian) => {
     createPage({
       path: `/${mondrian.id}`,
-      component: require.resolve('./src/pages/Mondrian.tsx'),
+      component: require.resolve('./src/templates/Mondrian.tsx'),
       context: {mondrian}
     })
   })
 
   createPage({
     path: '/',
-    component: require.resolve('./src/pages/Overview.tsx'),
+    component: require.resolve('./src/templates/Overview.tsx'),
     context: {mondrianList: mondrians}
   })
 }
